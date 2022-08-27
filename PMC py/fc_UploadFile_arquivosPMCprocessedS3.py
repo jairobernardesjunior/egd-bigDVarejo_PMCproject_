3b89bf9a5c76ee033b21b3f00b61ec3a90f7c4ad
jairo2016 <jairobernardesjunior@gmail.com>
2022-08-27 13:36:18 -0300

    v6.0
    v6.0

--- PMC py/fc_UploadFile_arquivosPMCprocessedS3.py
+++ PMC py/fc_UploadFile_arquivosPMCprocessedS3.py
@@ -4,7 +4,7 @@ import boto3
 def UploadFile_arquivosPMCprocessedS3(NomeBucketS3, nomeArquivo, pathArquivo):
     client = boto3.client(
         service_name='s3',
-        aws_access_key_id='xxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        aws_access_key_id='xxxxxxxxxxxxxxxxxxxxxxxxxxx',
         aws_secret_access_key='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
         region_name='eu-west-1' # voce pode usar qualquer regiao
         ) 
